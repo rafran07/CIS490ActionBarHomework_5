a67f7712026ac3922d2fbdc3cb28660065984460
rafran07 <rafran07@louisville.edu>
2015-10-25 16:49:04 -0400

    Initial commit

---
+++ app/src/main/java/app/androidactionbar/amal/com/actionbar/Second_Fragment.java
@@ -0,0 +1,23 @@
+package app.androidactionbar.amal.com.actionbar;
+
+
+import android.os.Bundle;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+
+public class Second_Fragment extends Fragment {
+    TextView text;
+    @Override
+    public View onCreateView(LayoutInflater inflater,ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.second_fragment, container, false);
+        text= (TextView) view.findViewById(R.id.txt);
+        String menu = getArguments().getString("Menu");
+        text.setText(menu);
+        return view;
+    }
+
+}
